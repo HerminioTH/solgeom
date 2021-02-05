407973febdd02026f532b89c0e67929bc975fcba
Herminio <herminio.eng@gmail.com>
2021-02-05 11:44:58 -0300

    A few comments on single_layer_column

--- Examples/single_layer_column.py
+++ Examples/single_layer_column.py
@@ -29,6 +29,7 @@ fig.subplots_adjust(left=0.070, right=0.975, top=0.970, bottom=0.065, hspace=0.2
 fonts = {'fontname': 'serif'}
 
 
+# Plot pressure profiles ---------------------------------------------------
 times_1 = [0., 1., 10., 30., 80.]
 for t in times_1:
 	p = terza.getPressureValuesConstTime(t)
@@ -36,29 +37,37 @@ for t in times_1:
 ax[0][0].set_xlabel("Pressure (kPa)", size=12, **fonts)
 ax[0][0].set_ylabel("Height (m)", size=12, **fonts)
 ax[0][0].grid(True)
+# --------------------------------------------------------------------------
 
+# Plot displacement profiles -----------------------------------------------
 for t in times_1:
 	w = terza.getDisplacementValuesConstTime(t)
 	ax[0][1].plot(w*mm, z)
 ax[0][1].set_xlabel("Displacement (mm)", size=12, **fonts)
 ax[0][1].set_ylabel("Height (m)", size=12, **fonts)
 ax[0][1].grid(True)
+# --------------------------------------------------------------------------
 
 
+
+
+# Plot bottom pressure over time -------------------------------------------
 times_2 = np.linspace(0, 200., 100)
-# for t in times_2:
 p = terza.getPressureValuesAtPosition(0.0, times_2) # Bottom pressure (z=0.0)
 ax[1][0].plot(times_2, p*kPa)
 ax[1][0].set_xlabel("Time (s)", size=12, **fonts)
 ax[1][0].set_ylabel("Bottom Pressure (kPa)", size=12, **fonts)
 ax[1][0].grid(True)
+# --------------------------------------------------------------------------
 
 
+# Plot top displacement over time ------------------------------------------
 w = terza.getDisplacementValuesAtPosition(height, times_2) # Top displacement (z=height)
 ax[1][1].plot(times_2, w*mm)
 ax[1][1].set_xlabel("Time (s)", size=12, **fonts)
 ax[1][1].set_ylabel("Top Displacement", size=12, **fonts)
 ax[1][1].grid(True)
+# --------------------------------------------------------------------------
 
 
 

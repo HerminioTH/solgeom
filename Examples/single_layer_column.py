afbaf0957c4384f49814cf377c42835d69332e66
Herminio <herminio.eng@gmail.com>
2021-02-08 08:31:40 -0300

    Minor modification

--- Examples/single_layer_column.py
+++ Examples/single_layer_column.py
@@ -9,7 +9,6 @@ mm = 1000.
 kPa = 1/1000.
 
 
-
 height = 10.0
 load = 1.0e+4
 gravity = 0.0
@@ -17,17 +16,13 @@ gravity = 0.0
 rock = json.load(open("solid.json", "r"))
 fluid = json.load(open("fluid.json", "r"))
 
-
-
 terza = Solution(height, load, rock, fluid , gravity)
 z = terza.getPositionValues()
-
 
 
 fig, ax = plt.subplots(2, 2, figsize=(8,7))
 fig.subplots_adjust(left=0.070, right=0.975, top=0.970, bottom=0.065, hspace=0.235, wspace=0.300)
 fonts = {'fontname': 'serif'}
-
 
 # Plot pressure profiles ---------------------------------------------------
 times_1 = [0., 1., 10., 30., 80.]

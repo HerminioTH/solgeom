48cdd7ff8f524bca87f842888b32de5d3650bd36
herminio <herminio.eng@gmail.com>
2021-02-08 22:49:25 -0300

    New modification

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='solgeom',
+    name='AnalyticalSolutionsForGeomechanics',
     version='0.0.1',
     description='My private package from private github repo',
     url='https://github.com/HerminioTH/AnalyticalSolutionsForGeomechanics.git',
     author='Hermínio T. Honório',
     author_email='herminio.eng@gmail.com',
     license='unlicense',
-    packages=['solgeom'],
+    packages=['AnalyticalSolutionsForGeomechanics'],
     zip_safe=False
 )
